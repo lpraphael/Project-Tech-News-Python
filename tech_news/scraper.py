ae9e9a47eb10e1a75903ed9c60734cea13402e0a
lpraphael <raphael.perazzo@gmail.com>
2022-11-10 17:35:21 -0300

    feat: implementa função que obtem as noticias e insere no banco de dados

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -24,14 +25,14 @@ def scrape_novidades(html_content: str) -> list:
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
+def scrape_next_page_link(html_content: str) -> str:
     selector = Selector(html_content)
 
     return selector.css('.next::attr(href)').get()
 
 
 # Requisito 4
-def scrape_noticia(html_content):
+def scrape_noticia(html_content: str) -> dict:
     selector = Selector(html_content)
 
     url = selector.css('link[rel=canonical]::attr(href)').get()
@@ -56,5 +57,24 @@ def scrape_noticia(html_content):
 
 
 # Requisito 5
-def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+def get_tech_news(amount: int) -> list:
+    url = 'https://blog.betrybe.com/'
+    all_news = []
+    n = len(all_news)
+
+    while n < amount:
+        html_content = fetch(url)
+        news_url = scrape_novidades(html_content)
+
+        for link in news_url:
+            if n == amount:
+                break
+            new_content = fetch(link)
+            all_news.append(scrape_noticia(new_content))
+            n += 1
+
+        url = scrape_next_page_link(html_content)
+
+    create_news(all_news)
+
+    return all_news
