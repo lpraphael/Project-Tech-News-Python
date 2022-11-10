4bb54ca0c449a944eebe517b34a2baf4c0eb7f12
lpraphael <raphael.perazzo@gmail.com>
2022-11-10 12:59:02 -0300

    feat: implementando função que pega o link da proxima págnia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,9 @@ def scrape_novidades(html_content: str) -> list:
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    return selector.css('.next::attr(href)').get()
 
 
 # Requisito 4
